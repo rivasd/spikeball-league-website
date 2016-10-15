805e1f54032b90049e5d238c647146290f20ba03
Daniel Rivas <rivasdaniel1992@gmail.com>
2016-10-14 20:12:10 -0400

    first model!

--- League/models.py
+++ League/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Player(models.Model):
+    """
+    Represents a single player in the league
+    """
+    
+    user = models.OneToOneField(User)
+    
